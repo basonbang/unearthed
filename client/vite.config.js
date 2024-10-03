090aefcfdd1cce72bee16e36dc256c2a461ca6d0
Jason <jasontang20043@gmail.com>
2024-10-03 16:35:16 -0700

    Feature: pasted in React code and update App.jsx useEffect

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,13 +1,12 @@
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  build: {
-    outDir: '../server/public',
-    emptyOutDir: true
-  },
-  server: {
-    proxy: {
-      '/gifts': {
+  plugins: [react()],
+  server: { 
+    proxy: { 
+      '/gifts': { 
         target: 'http://localhost:3001'
       }
     }
