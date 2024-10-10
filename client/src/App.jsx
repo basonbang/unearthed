e99796a9dd2d02c94aa7f1dea155de8a36a30e25
Jason <jasontang20043@gmail.com>
2024-10-10 16:15:10 -0700

    add create and edit pages with buttons on home page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,6 +4,8 @@ import { useRoutes } from 'react-router-dom'
 import Gifts from './pages/Gifts'
 import GiftDetails from './pages/GiftDetails'
 import PageNotFound from './pages/PageNotFound'
+import CreateGift from './pages/CreateGift';
+import EditGift from './pages/EditGift';
 import { Link } from 'react-router-dom'
 
 
@@ -41,6 +43,14 @@ const App = () => {
     {
       path:"/*",
       element: <PageNotFound />
+    },
+    {
+      path:"/new",
+      element: <CreateGift />
+    },
+    {
+      path:"/edit/:id",
+      element: <EditGift data={gifts}/>
     }
   ]);
 
@@ -57,6 +67,7 @@ const App = () => {
           </div>
           <div className="header-right">
             <Link to="/"><button className="homeBtn">Home</button></Link>
+            <Link to="/new"><button className='addBtn'>+ Add Gift</button></Link>
           </div>
         </div>
       </header>
